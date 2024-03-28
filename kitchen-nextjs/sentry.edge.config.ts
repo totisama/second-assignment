09c683ee50a5f21d745f769e55752b7733cde179
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-03-28 12:15:56 +0100

    Add sentry config

---
+++ kitchen-nextjs/sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://98fed965b101b223e907936174722cd4@o4506988321767424.ingest.us.sentry.io/4506988322684928",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
--- kitchen-nextjs/src/app/cocktails/page.tsx
+++ kitchen-nextjs/src/app/cocktails/page.tsx
@@ -28,7 +28,6 @@ export default async function CocktailsPage({
 
       <Suspense fallback={<CocktailsSectionSkeleton />}>
         <CocktailsSection cocktail={cocktail} />
-        {/* <CocktailsSectionSkeleton /> */}
       </Suspense>
     </main>
   )
